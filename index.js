52644a1f21e4acdb6aa156b13555f843f2eea496
howard.reith@gmail.com <howard.reith@gmail.com>
2021-10-16 17:19:48 -0400

    Serving static html file successfully

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const path = require('path');
+const addMainPageController = require('./routes/index');
+
+const port = process.env.PORT || 8080;
+const app = express();
+const httpServer = http.createServer(app);
+
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'public')))
+
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+
+addMainPageController(app);
+
+// Start the Server
+httpServer.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.info(`Server Started. Listening on *:${port}`);
+});
