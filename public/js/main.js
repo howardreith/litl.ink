800b211db3f0fe02c87d653d6915db08cc627f62
howard.reith@gmail.com <howard.reith@gmail.com>
2021-10-17 13:31:02 -0400

    Add styling. Clean some things up

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,53 @@
+'use strict';
+
+$("#errorWrapper").hide();
+$("#linkContainer").hide();
+$("#copyToClipboardButton").hide();
+
+$("#urlInputForm").submit((event) => {
+  event.preventDefault();
+  const value = document.getElementById('urlInput').value;
+  if (!isValidHttpUrl(value)) {
+    $("#errorWrapper").show();
+    $('#linkContainer').hide();
+    $('#copyToClipboardButton').hide();
+    throw Error('Invalid URL')
+  }
+
+  const backendUrl = window.location.href.includes('localhost') ?
+    'http://localhost:8080/addLink' :
+    'https://reithlitlink.herokuapp.com/addLink';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({url: value})
+  };
+  fetch(backendUrl, options)
+    .then((res) => res.json()
+    .then((res) => {
+    $('#linkContainer').show();
+    $('#copyToClipboardButton').show();
+    const link = res.builtUrl;
+    document.getElementById("littleLink").innerHTML =
+      `<a id="littleLink" class="littleLink" href="${link}" target=_blank>${link.replace('http://', '')}</a>`;
+    $("#errorWrapper").hide();
+  }))
+    .catch((e) => {
+      console.error('An error occurred while fetching link: ', e)
+    })
+});
+
+$('#copyToClipboardButton').click(() => {
+  const littleUrl = document.getElementById("littleLink").innerText;
+  navigator.clipboard.writeText(littleUrl)
+    .catch((e) => {
+      console.error('An error occurred while writing text to clipboard: ', e)
+    })
+});
+
 function isValidHttpUrl(string) {
   let url;
 
@@ -9,28 +59,3 @@ function isValidHttpUrl(string) {
 
   return url.protocol === "http:" || url.protocol === "https:";
 }
-
-$("#urlInputForm").submit((event) => {
-  event.preventDefault();
-  const value = document.getElementById('urlInput').value
-  if (!isValidHttpUrl(value)) {
-    document.getElementById("linkContainer").innerHTML =
-      `<div id="linkContainer"><span>Invalid URL</span></div>`;
-    throw Error('Invalid URL')
-  }
-
-  const backendUrl = window.location.href.includes('localhost') ? 'http://localhost:8080/addLink' : 'https://reithlitlink.herokuapp.com/addLink'
-  const options = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({url: value})
-  };
-  fetch(backendUrl, options).then((res) => res.json().then((res) => {
-    const link = res.builtUrl
-    document.getElementById("linkContainer").innerHTML =
-      `<div id="linkContainer"><a href="${link}" target=_blank>${link.replace('http://', '')}</a></div>`;
-  }))
-});
